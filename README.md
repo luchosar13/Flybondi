76a974e39c7303d5e162340cc7b2a0b2fa8d1a29
luchosar13 <101670221+luchosar13@users.noreply.github.com>
2023-11-17 12:48:17 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flybondi
+Desafio de Flybondi donde se nos pide realizar una interfaz para comprar pasajes de vuelo. 
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {dataset as data} from '../dataset';
+import {useState,useEffect} from 'react';
+import InfoVuelo from './components/InfoVuelo'
+
+function App() {
+  const [info, setInfo] = useState([])
+
+  useEffect(() => {
+    setInfo(data)
+  
+  }, [])
+  
+
+  return (
+   <div>
+    <h1>¡Vuela ya mismo con Flybondi!</h1>
+    <InfoVuelo info={info}/>
+   </div>
+  )
+}
+
+export default App
